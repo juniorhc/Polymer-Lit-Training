9f84657be57b16f222ff26a5843759b67faff4ec
Junior <junior.jaava@gmail.com>
2019-12-06 20:04:01 +0100

    first full commit to keep the track

---
+++ public/js/components/ContentArea.js
@@ -0,0 +1,47 @@
+import { LitElement, html } from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
+import ContactsList  from '/js/components/ContactsList.js';
+import FavoritesList  from '/js/components/FavoritesList.js';
+import FormPopup  from '/js/components/FormPopup.js';
+
+export default class ContentArea extends LitElement {
+    constructor() {
+        super();
+        this.total = {
+            number: 0
+        };
+    }
+
+    static get properties() {
+        return {
+            total: Object,
+            popupOpen: Boolean
+        }
+    }
+
+
+    firstUpdated(changedProperties) {
+        
+    }
+
+    render() {
+        const { color, background, fontWeight } = this;
+        return html`
+        <style>
+            @import '/css/global.css';
+            #content-area{
+                background: #fcfdff;
+                padding: 50px 80px;
+            }
+        </style>
+        <section id="content-area">
+            <form-popup .popupOpen="${this.popupOpen}"></form-popup>
+            <favorites-list></favorites-list>
+            <contacts-list></contacts-list>
+        </section>
+`
+    }
+
+
+}
+
+customElements.define('content-area', ContentArea);
---
+++ public/js/components/SideMenu.js
@@ -0,0 +1,77 @@
+import { LitElement, html } from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
+
+export default class SideMenu extends LitElement {
+    constructor() {
+        super();
+        this.total = {
+            number: 0
+        };
+    }
+
+    static get properties() {
+        return {   
+            addContactClick: Function
+        }
+    }
+
+
+    firstUpdated(changedProperties) { 
+
+    }
+
+    render() {
+        return html`
+<style>
+        @import '/css/global.css';
+        #side-menu{
+            background: #323759;
+            height: 100vh;
+            padding: 50px 25px;
+        }
+
+        .logo{
+            text-align: center;
+        }
+        .logo img{
+            width: 50px;
+        }
+        .title{
+            font-weight: 700;
+            color: #ccced7;
+            margin: 1rem 0;
+        }
+        #side-menu nav a{
+            color: #ccced7;
+            text-decoration: none;
+            text-transform: capitalize;
+            display: block;    
+            padding: 10px 10px 0;
+        }
+        #side-menu nav a span.icon{
+            padding: 10px 10px 10px 0;
+        }
+</style>
+
+<section id="side-menu">
+    <div class="logo">
+        <img src="https://icon-library.net/images/icon-png-transparent-background/icon-png-transparent-background-6.jpg" />
+    </div>
+    <div class="menu">
+    <div class="title"> Contacts </div>
+    <nav>
+        <a href="#" @click="${this.addContactClick}"><span class="icon"> + </span> Add Contact </a>
+        <a href="#"><span class="icon"> + </span> Add Contact </a>
+        <a href="#"><span class="icon"> + </span> Add Contact </a>
+        <a href="#"><span class="icon"> + </span> Add Contact </a>
+    </nav>
+    </div>
+</section>
+
+
+               `
+    }
+
+
+}
+
+customElements.define('side-menu', SideMenu);
