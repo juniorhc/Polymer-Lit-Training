ef32a70c3f5adb6fb011b29193c8fdad1aad6e84
Junior <junior.jaava@gmail.com>
2019-12-13 20:50:42 +0100

    + toggle popup to show and disable + passing arrays between many component + form to add info related to contact + save favoritee

--- public/js/components/ContentArea.js
+++ public/js/components/ContentArea.js
@@ -19,7 +19,8 @@ export default class ContentArea extends LitElement {
                 notify: false
             },
             togglePopup: Function,
-            saveContact: Function
+            saveContact: Function,
+            allContacts: Array
         }
     }
 
@@ -40,8 +41,8 @@ export default class ContentArea extends LitElement {
         </style>
         <section id="content-area">
             <form-popup .popupOpen="${this.popupOpen}" .togglePopup="${this.togglePopup}" .saveContact="${this.saveContact}"></form-popup>
-            <favorites-list></favorites-list>
-            <contacts-list></contacts-list>
+            <favorites-list .allContacts=${this.allContacts}></favorites-list>
+            <contacts-list .allContacts=${this.allContacts}></contacts-list>
         </section>
 `
     }
